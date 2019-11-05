d91211e99e43e4b14c723b4603973065bac07f3a
besigye <jamesbesigyek@gmail.com>
2019-11-05 09:51:16 +0300

    completed hallo world

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 const express = require("express");
 const app = express();
-/* created an app of express object, then  listening on port
+/* created an app of express object, then  listening on port.
 listen method takes two parameters, port number and function*/
 app.listen(5000,function(){
     console.log("listening on port 5000")
+   // app.get(path,callback())- using the get method
+app.get("/", function(req,res){
+    res.send("Hello World- The Node Js App is here")
+})
 })
