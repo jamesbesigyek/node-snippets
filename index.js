ec32f181b2211510130ccf257ed66a9ff526506c
besigye <jamesbesigyek@gmail.com>
2019-11-05 09:29:39 +0300

    created app of object express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+//console.log("hello world") used to test the initial setup
+
+const express = require("express");
+const app = express();
+/* created an app of express object, then  listening on port
+listen method takes two parameters, port number and function*/
+app.listen(5000,function(){
+    console.log("listening on port 5000")
+})
