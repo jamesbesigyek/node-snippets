6acee7f9714907f736d9cb0d956da0f29b334af8
besigye <jamesbesigyek@gmail.com>
2019-11-06 09:19:42 +0300

    Routing in Process for get method

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require("express");
 const app = express();
 /* created an app of express object, then  listening on port.
 listen method takes two parameters, port number and function*/
-app.listen(5000,function(){
+app.listen(5000,()=>{                           //arrow function
     console.log("listening on port 5000")
    // app.get(path,callback())- using the get method
 app.get("/", function(req,res){
     res.send("Hello World- The Node Js App is here")
+})
+app.get("/about", function(req,res){
+    res.send("Hello World- This is the about space")
 })
 })
