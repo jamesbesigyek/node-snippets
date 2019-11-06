e2362a076b4c0e64039ff10799fef608458e9bf3
besigye <jamesbesigyek@gmail.com>
2019-11-06 10:46:26 +0300

    added query parameters

--- index.js
+++ index.js
@@ -7,10 +7,30 @@ listen method takes two parameters, port number and function*/
 app.listen(5000,()=>{                           //arrow function
     console.log("listening on port 5000")
    // app.get(path,callback())- using the get method
-app.get("/", function(req,res){
+app.get("/", (req,res)=>{
     res.send("Hello World- The Node Js App is here")
 })
-app.get("/about", function(req,res){
-    res.send("Hello World- This is the about space")
+app.get("/about", (req,res)=>{        //get method used for rendering-- reading from server and displaying on
+    res.send("Hello World- This is the about space") //browser 
+
+})
+app.post("/post", (req,res)=>{        //get method used for rendering-- reading from server and displaying on
+    res.send("Hello World- got a post request") //browser 
+})
+app.put("/put", (req,res)=>{        //get method used for rendering-- reading from server and displaying on
+    res.send("Hello World- Got a put request") //browser 
+})
+app.delete("/delete", (req,res)=>{        //get method used for rendering-- reading from server and displaying on
+    res.send("Hello World- Got to delete a request") //browser 
+})
+
+app.get("/user/:name", (req,res)=>{
+    res.send("Hello" + req.params.name) //query parameter
+})
+
+/* The error--Asterix-- below should always be the last*/
+
+app.get("*", (req,res)=>{
+    res.send("wrong path, try another one")
 })
 })
